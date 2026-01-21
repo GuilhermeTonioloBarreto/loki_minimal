1af3e2afddcbf1c65ca446f25d37082060f9feb3
Guilherme Toniolo Barreto <domtoniolo@gmail.com>
2026-01-20 22:48:29 -0300

    inserção da pasta e6 que explica como usar consultas no banco de dados com regex

---
+++ e6/post-eps-M1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+jq -n '{
+  streams: [
+    {
+      stream: {
+        job: "quati",
+        service: "eps",
+        level: "info",
+        env: "dev"
+      },
+      values: [
+        [ ((now * 1000000000) | floor | tostring), "M1 está com problema" ]
+      ]
+    }
+  ]
+}' | curl -X POST http://localhost:3100/loki/api/v1/push \
+  -H "Content-Type: application/json" \
+  -d @-
